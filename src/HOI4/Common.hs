4982664d95ec9cbb454c6b23ca1be5acc9021d96
Simca <mmosimca@gmail.com>
2026-08-29 12:35:00 -0400

    Fixed effect-matching being case sensitive.

--- src/HOI4/Common.hs
+++ src/HOI4/Common.hs
@@ -1094,12 +1094,20 @@ extractStmt p xs = extractStmt' p xs []
             else
                 extractStmt' p xs (acc++[x])
 
--- | Predicate for matching text on the left hand side
+-- | Predicate for matching text on the left hand side. The game reads a key
+-- however it happens to be capitalized, and script now and then writes one
+-- oddly -- @originaL_tag_to_check@ in the Australian tree -- so a key is
+-- matched the same way here. A field this misses is not read at all.
 matchLhsText :: Text -> GenericStatement -> Bool
-matchLhsText t [pdx| $lhs = %_ |] | t == lhs = True
-matchLhsText t [pdx| $lhs < %_ |] | t == lhs = True
-matchLhsText t [pdx| $lhs > %_ |] | t == lhs = True
+matchLhsText t [pdx| $lhs = %_ |] | sameKey t lhs = True
+matchLhsText t [pdx| $lhs < %_ |] | sameKey t lhs = True
+matchLhsText t [pdx| $lhs > %_ |] | sameKey t lhs = True
 matchLhsText _ _ = False
+
+-- | Whether two script keys are the same one, which is not a matter of how
+-- either is capitalized.
+sameKey :: Text -> Text -> Bool
+sameKey a b = T.toLower a == T.toLower b
 
 -- | Predicate for matching text on boths sides
 matchExactText :: Text -> Text -> GenericStatement -> Bool
